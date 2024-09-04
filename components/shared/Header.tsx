198ad7992a23f1c0fac921a7d075f16c45311244
yakshb <yakshb@outlook.com>
2024-09-03 23:23:45 -0400

    "Update Header component: changed logo asset, adjusted logo size, and modified navigation bar padding"

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -5,7 +5,7 @@ import { ChevronsUp, Info, MessageSquare } from "lucide-react";
 import ThemeToggleButton from "./ThemeToggle";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-import logo from "@/assets/TOLai-logo.svg";
+import logo from "@/assets/GENOSPHERE.svg";
 import { Button } from "@/components/ui/button";
 // import { FeedbackModal } from "./FeedbackModal";
 import AboutModal from "./AboutModal";
@@ -15,7 +15,7 @@ const Header = () => {
 
   return (
     <nav className="sticky top-0 z-50 backdrop-filter bg-white/80 dark:bg-black/80 backdrop-blur-lg bg-opacity-50 border-b-[#343D3F]">
-      <div className="mx-auto flex items-center justify-between px-2 py-4 md:px-12">
+      <div className="mx-auto flex items-center justify-between px-2 py-2 md:px-12">
         <Link
           className="gap-3 flex flex-row align-middle items-center group text-2xl md:text-3xl font-semibold tracking-tight"
           href="/"
@@ -23,10 +23,10 @@ const Header = () => {
           <Image
             src={logo}
             alt="Tree of Life Explorer AI Logo"
-            width={40}
-            height={40}
+            width={250}
+            height={100}
           />
-          Genosphere
+          
         </Link>
 
         <div className="flex items-center space-x-2 md:space-x-4">
